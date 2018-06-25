a6e84c05ae7f7cb0db7c8107b0bd1bb81ea996dc
Serkan Özdemir <37292763+je8n@users.noreply.github.com>
2018-06-25 17:50:30 +0300

    Update Initializer.java

--- WebService/Source Packages/com.mycompany.webservice.appclass/Initializer.java
+++ WebService/Source Packages/com.mycompany.webservice.appclass/Initializer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.webservice.appclass;
+package com.appclass;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
